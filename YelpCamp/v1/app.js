8dcb88c801c9fcbf49f8a20393f896af2aea8ac0
Jason Leong <soundslikejason@users.noreply.github.com>
2017-01-23 02:18:53 +0000

    push from cloud 9

---
+++ YelpCamp/v1/app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res) {
+    var campgrounds = [
+        {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7258/7121861565_3f4957acb1.jpg"},    
+        {name: "Granite Hill", image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg"},    
+        {name: "Mountain Goat's Rest", image: "https://farm8.staticflickr.com/7259/7121858075_7375241459.jpg"}    
+    ]
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("The YelpCamp Server Has Started!");
+});
---
+++ YelpCamp/v1/readme.md
@@ -0,0 +1,16 @@
+#YelpCamp
+
+* Add Landing Page
+* Add Campgrounds Page that lists all campgrounds
+
+Each Campground has:
+ * Name
+ * Image
+
+How to begin:
+npm init
+npm install express ejs --save
+touch app.js
+
+mkdir views
+touch view/landing.ejs
